add245deafa0cc9c84f8692f7960d1ececf8fb49
Eldin <eldinsmakic@hotmail.fr>
2022-06-26 22:17:39 +0200

    Initial Commit

---
+++ TransactionScreen/TransactionScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  TransactionScreenApp.swift
+//  TransactionScreen
+//
+//  Created by Eldin SMAKIC on 26/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct TransactionScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
