bbabe8d70e17768581f8f31246a0f1d9b69e5d14
Eldin <eldinsmakic@hotmail.fr>
2022-06-28 10:47:20 +0200

    [Feat](View): Add Presenter

--- TransactionScreen/TransationsVIew.swift
+++ TransactionScreen/TransationsVIew.swift
@@ -9,7 +9,7 @@ import SwiftUI
 import BudgetPlannerCore
 
 struct TransationsView: View {
-    @StateObject private var transactionPresenter = TransactionPresenter()
+    @StateObject private var presenter = TransactionPresenter()
     @State var list: [TransactionViewModel] = []
     
     @State var isModalActivated = false
